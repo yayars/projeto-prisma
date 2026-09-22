d4c6c28d09e6afbcd6b8149defb7c428a523e38f
yayars <yasmin.silva2@aluno.ifsp.edu.br>
2026-09-22 17:40:42 -0300

    completo

---
+++ prisma/migrations/20260922201312_criar_produtos/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "preco" REAL NOT NULL,
+    "estoque" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
